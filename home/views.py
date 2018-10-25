48db5c019daf85afecd37568d0f34b3c5f1e1b7f
mkramer0820 <mkramer0820@gmail.com>
2018-10-25 17:41:32 -0400

    auth

--- home/views.py
+++ home/views.py
@@ -14,6 +14,66 @@ class Logout(LogoutView):
 
 def index(request):
     return render(request, 'index.html')
-
+"""
 
 # Create your views here.
+@api_view(['POST'])
+@permission_classes([AllowAny, ])
+def authenticate_user(request):
+    try:
+        email = request.data['email']
+        password = request.data['password']
+
+        user = User.objects.get(email=email, password=password)
+        if user:
+            try:
+                payload = jwt_payload_handler(user)
+                token = jwt.encode(payload, settings.SECRET_KEY)
+                user_details = {}
+                user_details['name'] = "%s %s" % (
+                    user.first_name, user.last_name)
+                user_details['token'] = token
+                user_logged_in.send(sender=user.__class__,
+                                    request=request, user=user)
+                return Response(user_details, status=status.HTTP_200_OK)
+
+            except Exception as e:
+                raise e
+        else:
+            res = {
+                'error': 'can not authenticate with the given credentials or the account has been deactivated'}
+            return Response(res, status=status.HTTP_403_FORBIDDEN)
+    except KeyError:
+        res = {'error': 'please provide a email and a password'}
+        return Response(res)
+        
+        
+        
+2
+user_logged_in.send(sender=user.__class__,
+                                    request=request, user=user)
+                                    
+                                    class UserRetrieveUpdateAPIView(RetrieveUpdateAPIView):
+ 
+    # Allow only authenticated users to access this url
+    permission_classes = (IsAuthenticated,)
+    serializer_class = UserSerializer
+ 
+    def get(self, request, *args, **kwargs):
+        # serializer to handle turning our `User` object into something that
+        # can be JSONified and sent to the client.
+        serializer = self.serializer_class(request.user)
+ 
+        return Response(serializer.data, status=status.HTTP_200_OK)
+ 
+    def put(self, request, *args, **kwargs):
+        serializer_data = request.data.get('user', {})
+ 
+        serializer = UserSerializer(
+            request.user, data=serializer_data, partial=True
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+ 
+        return Response(serializer.data, status=status.HTTP_200_OK)
+"""
